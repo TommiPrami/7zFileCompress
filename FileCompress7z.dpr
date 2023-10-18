77f2e28d3b60756276cdac70b6a5a4b323991d73
Tommi Prami <tommi.prami@gmail.com>
2023-10-18 13:33:02 +0300

    Initial CheckIn

---
+++ FileCompress7z.dpr
@@ -0,0 +1,41 @@
+﻿program FileCompress7z;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils, System.IOUtils;
+
+procedure PrintHelp;
+begin
+  WriteLn('FileCompress7z RootPath SearchPattern');
+  WriteLn('  FileCompress7z "C:\Temp\" *.largefile');
+  WriteLn('');
+end;
+
+var
+  LRootFolder: string;
+  LFileType: string;
+begin
+  try
+    LRootFolder := IncludeTrailingPathDelimiter(ParamStr(1));
+    LFileType := ParamStr(2);
+
+    if FileExists(LRootFolder) and not LFileType.IsEmpty then
+    begin
+      //
+      TDirectory.GetFiles(LRootFolder, LFileType, TSearchOption.soTopDirectoryOnly);
+
+    end
+    else
+    begin
+      PrintHelp;
+      Exit;
+    end;
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
